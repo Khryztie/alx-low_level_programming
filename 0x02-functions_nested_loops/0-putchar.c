2aea4551ac1125bac7654354e8bd0dce802b0391
Khryztie <ekechristiana@gmail.com>
2023-04-26 03:42:31 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,31 @@
 #include <time.h>
 /**
  * main - Entry point
+ * Description: 'prints _putchar to stdout'
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
+}
 
-printf("_putchar\n");
+
+
+
+
+
+
 
 return (0);
 }
